05c4f4acfc8b84158bd7d677b6ddf620c6f2e458
Joseph Martin <josephm@maine.rr.com>
2016-03-27 18:24:38 -0400

    remove dotenv (not necessary for example)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 #![feature(custom_derive, custom_attribute, plugin)]
-#![plugin(diesel_codegen, dotenv_macros)]
+#![plugin(diesel_codegen)]
 
 #[macro_use]
 extern crate diesel;
-extern crate dotenv;
 
 infer_schema!("postgres://localhost/enum_type_test");
 
